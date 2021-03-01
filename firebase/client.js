ae478c9c80c48d479c52caeb8c22482b007317e9
Raul Zarza <raulzarza.dev@gmail.com>
2021-03-01 12:39:06 -0600

    Ahora puedes ver la configuracion de firebase

--- firebase/client.js
+++ firebase/client.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCR4wS0ayiiLuYJdWg9kv6ZynsEvykvVL0",
-  authDomain: "negocios-cc009.firebaseapp.com",
-  projectId: "negocios-cc009",
-  storageBucket: "negocios-cc009.appspot.com",
-  messagingSenderId: "875306027522",
-  appId: "1:875306027522:web:ee0c56738fcd950f1db51b",
-  measurementId: "G-PMNC54B6TS",
-};
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -22,10 +14,10 @@ const mapUserFromFirebase = (user) => {
 
 export const onAuthStateChanged = (onChange) => {
   return firebase.auth().onAuthStateChanged((user) => {
-    const normalizeUser = mapUserFromFirebase(user)
-    onChange(normalizeUser)
+    const normalizeUser = mapUserFromFirebase(user);
+    onChange(normalizeUser);
   });
-}; 
+};
 
 export const loginWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
