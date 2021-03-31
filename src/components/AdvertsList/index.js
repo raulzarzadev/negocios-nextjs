41e56b31ce26846ffbc8f47998edeef7a06c5c32
Raul Zarza <raulzarza.dev@gmail.com>
2021-03-30 21:35:40 -0700

    ahora el responsive fuciona mejor en barrios list

--- src/components/AdvertsList/index.js
+++ src/components/AdvertsList/index.js
@@ -6,6 +6,7 @@ export default function AdvertsList({ barrio = undefined, adverts }) {
     <div className={styles.barrio}>
       <div className={styles.filter}></div>
       <h2 className={styles.title}>{barrio?.name}</h2>
+      
       <div className={styles.adverts}>
         {adverts?.map((ad, i) => (
           <div key={i} className={styles.advert}>
